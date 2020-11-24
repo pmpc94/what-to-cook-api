ea3d4fe0cb98e7accd74882522618c67664a0a5a
pmpc94 <pmpcwork@gmail.com>
2020-11-24 22:04:00 +0000

    add initial seeder files

---
+++ database/seeds/IngredientSeeder.js
@@ -0,0 +1,21 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| IngredientSeeder
+|--------------------------------------------------------------------------
+|
+| Make use of the Factory instance to seed database with dummy data or
+| make use of Lucid models directly.
+|
+*/
+
+/** @type {import('@adonisjs/lucid/src/Factory')} */
+const Factory = use('Factory')
+
+class IngredientSeeder {
+  async run () {
+  }
+}
+
+module.exports = IngredientSeeder
---
+++ database/seeds/RecipySeeder.js
@@ -0,0 +1,21 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| RecipySeeder
+|--------------------------------------------------------------------------
+|
+| Make use of the Factory instance to seed database with dummy data or
+| make use of Lucid models directly.
+|
+*/
+
+/** @type {import('@adonisjs/lucid/src/Factory')} */
+const Factory = use('Factory')
+
+class RecipySeeder {
+  async run () {
+  }
+}
+
+module.exports = RecipySeeder
